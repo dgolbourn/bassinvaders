f6dc9436728a95354e22f778042a5f516ddfec8c
Darren Golbourn <golbourn@gmail.com>
2014-01-05 19:42:28 +0000

    put x and y coordinates into single model

--- src/game/dynamics.cc
+++ src/game/dynamics.cc
@@ -13,10 +13,13 @@ class Model
 {
 public:
   float x_;
+  float y_;
+  float u_;
   float v_;
   void Step(void)
   {
-    x_ += v_ * float(dt);
+    x_ += u_ * float(dt);
+    y_ += v_ * float(dt);
   }
 };
 
@@ -24,7 +27,7 @@ class DynamicsImpl
 {
 public:
   sdl::Mutex mutex_;
-  std::pair<Model, Model> model_;
+  Model model_;
   event::Signal signal_;
   event::Timer timer_;
   DynamicsImpl(float x, float y, float u, float v);
@@ -40,24 +43,23 @@ void DynamicsImpl::Add(Dynamics::Command command)
   auto bind = [command, this](void)
   {
     sdl::Lock(this->mutex_);
-    return command(Dynamics::Position(this->model_.first.x_, this->model_.second.x_));
+    return command(Dynamics::Position(this->model_.x_, this->model_.y_));
   };
   signal_.Add(bind);
 }
 
 void DynamicsImpl::Step(void)
 {
-  model_.first.Step();
-  model_.second.Step();
+  model_.Step();
   signal_();
 }
 
 DynamicsImpl::DynamicsImpl(float x, float y, float u, float v) : timer_(dt)
 {
-  model_.first.x_ = x;
-  model_.first.v_ = u;
-  model_.second.x_ = y;
-  model_.second.v_ = v;
+  model_.x_ = x;
+  model_.u_ = u;
+  model_.y_ = y;
+  model_.v_ = v;
 }
 
 void DynamicsImpl::Play(void)
@@ -89,49 +91,49 @@ Dynamics::Dynamics(float x, float y, float u, float v)
 void Dynamics::x(float x)
 {
   sdl::Lock(impl_->mutex_);
-  impl_->model_.first.x_ = x;
+  impl_->model_.x_ = x;
 }
 
 void Dynamics::y(float y)
 {
   sdl::Lock(impl_->mutex_);
-  impl_->model_.second.x_ = y;
+  impl_->model_.y_ = y;
 }
 
 void Dynamics::u(float u)
 {
   sdl::Lock(impl_->mutex_);
-  impl_->model_.first.v_ = u;
+  impl_->model_.u_ = u;
 }
 
 void Dynamics::v(float v)
 {
   sdl::Lock(impl_->mutex_);
-  impl_->model_.second.v_ = v;
+  impl_->model_.v_ = v;
 }
 
 float Dynamics::x(void) const
 {
   sdl::Lock(impl_->mutex_);
-  return impl_->model_.first.x_;
+  return impl_->model_.x_;
 }
 
 float Dynamics::y(void) const
 {
   sdl::Lock(impl_->mutex_);
-  return impl_->model_.second.x_;
+  return impl_->model_.y_;
 }
 
 float Dynamics::u(void) const
 {
   sdl::Lock(impl_->mutex_);
-  return impl_->model_.first.v_;
+  return impl_->model_.u_;
 }
 
 float Dynamics::v(void) const
 {
   sdl::Lock(impl_->mutex_);
-  return impl_->model_.second.v_;
+  return impl_->model_.v_;
 }
 
 void Dynamics::Play(void)
